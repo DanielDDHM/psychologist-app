6f867fd0992bf03eb142d3802f81031fdee7342d
DanielDDHM <daniel.ddhm@gmail.com>
2022-06-29 13:22:12 -0300

    feat: initial docs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import cors from 'cors';
-import express, { Request, Response } from 'express';
 import "express-async-errors";
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import routes from './routes';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import express, { Request, Response } from 'express';
 const { PORT, DATABASE_URL } = process.env
+import { Documentation } from './docs/api';
 
 //APP
 const app = express();
@@ -20,11 +22,16 @@ app.get('/', (req: Request, res: Response) => {
   return res.send({ status: 'UP' });
 });
 
+// DOCS
+app.use("/api-docs",
+  swaggerUi.serve, swaggerUi.setup(Documentation.Api))
+
 mongoose.connect(String(DATABASE_URL))
   .then(() => {
     app.listen(PORT, () => {
       console.log(`APP DB: ${DATABASE_URL}`)
       console.log(`APP STARTED ON http://localhost:${PORT || 3000}`);
+      console.log(`API DOCS: http://localhost:${PORT || 3000}/api-docs`);
     });
   })
   .catch(e => console.error('COULD NOT CONNECT ON DB', e))
---
+++ src/docs/api.ts
@@ -0,0 +1,35 @@
+export namespace Documentation {
+  export const Api = {
+    openapi: "3.0.3",
+    info: {
+      title: "BET",
+      description: "API for a bet animal game",
+      version: "1.0.0",
+      contact: {
+        email: "daniel.ddhm@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/',
+        description: 'Local Server',
+      }
+    ],
+    tags: [
+      { name: 'Auth' },
+      { name: 'Call' },
+      { name: 'Chat' },
+      { name: 'Consults' },
+      { name: 'Diagnostics' },
+      { name: 'Psychologists' },
+      { name: 'Staff' },
+      { name: 'Users' },
+    ],
+
+    paths: {},
+    components: {
+      schemas: {},
+      securitySchemes: {},
+    },
+  }
+}
